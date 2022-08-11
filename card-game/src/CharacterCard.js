125f5a1126ccf904adb370f2158df0eb0ace80c9
Chualchai Apichatitiworn <whatbest@MacBook-Pro-khxng-Chualchai.local>
2022-08-11 16:57:01 +0700

    part 6
    part 6

--- card-game/src/CharacterCard.js
+++ card-game/src/CharacterCard.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 export default function CharacterCard(props) {
-
+    const attemptRef = useRef(props.attempt);   
     const [active, setActive] = useState(false);
     const activate = () => {
         if(!active){
             setActive(true)
             props.activationHandler(props.value)
             }
- }
+        }
+        useEffect(() => {
+           if(attemptRef.current != props.attempt){
+              setActive(false)
+              attemptRef.current = props.attempt
+           }
+       })       
  
  const className = `card ${active ? 'activeCard': ''}`
  return (
